eb2978ad954d2e43da34528956eaf35389902822
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2022-10-13 16:02:26 +0200

    new logo

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <nav class="navbar navbar-expand-lg navbar-dark">
     <div class="container-fluid">
       <router-link to="/" class="navbar-brand">
-        <img src="../assets/logo.png" alt="" width="30" class="d-inline-block align-bottom navbar-img">
+        <img src="../assets/logo.png" alt="" width="30" class="d-inline-block align-baseline navbar-img">
         Songbird Domains
       </router-link>
 

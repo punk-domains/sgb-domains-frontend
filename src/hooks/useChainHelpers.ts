355fe185815788a2326c122d5d1bc107ddc996fc
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2022-10-17 00:18:02 +0200

    new rpc urls added

--- src/hooks/useChainHelpers.ts
+++ src/hooks/useChainHelpers.ts
@@ -54,7 +54,8 @@ export default function useChainHelpers() {
       } else if (networkId === 10) {
         // Optimism
         urls = [
-          "https://mainnet.optimism.io"
+          "https://1rpc.io/op",
+          "https://rpc.ankr.com/optimism"
         ]; 
       } else if (networkId === 19) {
         // Songbird
@@ -150,7 +151,7 @@ export default function useChainHelpers() {
         chainId: "0xA",
         chainName: "Optimism",
         nativeCurrency: { decimals: 18, name: "ETH", symbol: "ETH" }, 
-        rpcUrls: ["https://mainnet.optimism.io"]
+        rpcUrls: ["https://1rpc.io/op", "https://rpc.ankr.com/optimism"]
       }] 
     } else if (networkName == "Optimism Testnet") {
       method = "wallet_addEthereumChain"

f0667cfce9b8016b4e09447c8e14be6622f40e96
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2023-03-02 00:16:03 +0100

    towolabs RPC removed

--- src/hooks/useChainHelpers.ts
+++ src/hooks/useChainHelpers.ts
@@ -65,7 +65,7 @@ export default function useChainHelpers() {
       } else if (networkId === 19) {
         // Songbird
         urls = [
-          "https://songbird.towolabs.com/rpc",
+          //"https://songbird.towolabs.com/rpc",
           "https://songbird-api.flare.network/ext/C/rpc"
         ]; 
       } else if (networkId === 56) {
@@ -239,7 +239,7 @@ export default function useChainHelpers() {
         chainId: "0x13",
         chainName: "Songbird",
         nativeCurrency: { decimals: 18, name: "SGB", symbol: "SGB" }, 
-        rpcUrls: ["https://songbird.towolabs.com/rpc", "https://songbird-api.flare.network/ext/C/rpc"]
+        rpcUrls: ["https://songbird-api.flare.network/ext/C/rpc"]
       }] 
     } else if (networkName == "Coston Testnet") {
       method = "wallet_addEthereumChain"
